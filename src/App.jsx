80f7feb9ec33242043b5afe5fb3d342986e7baca
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-15 03:53:06 +0900

    Feat: 로그인 관련 navigate 링크 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Welcome from './components/Welcome/Welcome';
 
 import './styles/global.scss';
 import './styles/variables.scss';
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
