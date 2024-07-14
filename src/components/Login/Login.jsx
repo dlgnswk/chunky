f12e46b230266f1f66665319105b485513578c7d
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-15 02:29:43 +0900

    Feat: 회원가입 버튼 클릭 시 회원가입 페이지 이동 구현

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import MainButton from '../shared/Button/MainButton';
 import Container from '../shared/Container/Container';
 import Logo from '../shared/Logo/Logo';
@@ -6,24 +7,28 @@ import LoginInput from './LoginInput';
 import './style.scss';
 
 function Login() {
+  const navigate = useNavigate();
+
   return (
-    <div className="wrapper">
+    <>
       <div className="login-logo">
         <Logo />
         <p>쉽고 편한 3D 라이프</p>
       </div>
-      <Container>
-        <div className="input-container">
-          <LoginInput text="ID" />
-          <LoginInput text="Password" />
-        </div>
-        <MainButton text="Login" />
-        <div className="text-button">
-          <span>회원이 아니세요? </span>
-          <span className="text-button-title">Join</span>
-        </div>
-      </Container>
-    </div>
+      <div className="wrapper">
+        <Container>
+          <div className="input-container">
+            <LoginInput text="ID" />
+            <LoginInput text="Password" />
+          </div>
+          <MainButton text="Login" />
+          <button className="text-button" onClick={() => navigate('/register')}>
+            <span>회원이 아니세요? </span>
+            <span className="text-button-title">Join</span>
+          </button>
+        </Container>
+      </div>
+    </>
   );
 }
 
