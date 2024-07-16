ac0562cbc9e54a59c4ccd3604fa000d7d1e5c36b
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-17 01:16:41 +0900

    Add: 3D 렌더링 뷰 아이콘 컴포넌트화

---
+++ src/components/shared/Icon/viewFrontIcon.jsx
@@ -0,0 +1,21 @@
+function viewFrontIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 17V6.5M3 17H13.5M3 17L6.5 13.5M3 6.5L6.5 3M3 6.5H13.5M6.5 3H17M6.5 3V13.5M17 3V13.5M17 3L13.5 6.5M17 13.5L13.5 17M17 13.5H6.5M13.5 17V6.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M13 7V17H3V7H13Z" fill="currentColor" />
+    </svg>
+  );
+}
+
+export default viewFrontIcon;
