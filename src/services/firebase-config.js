34422e315d26b77905d3552d69023dbb47047b9f
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-18 00:03:41 +0900

    Feat: 로그인 관련 기능을 위한 기능 추가
    - 로그인 여부 감시를 위한 onAuthStateChanged 이벤트 리스너를 import 하였습니다.
    - google 로그인 연동을 위한 googleAuthProvider를 import하였습니다.

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
@@ -17,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { auth, db, storage };
+export { auth, db, storage, onAuthStateChanged, googleProvider };
