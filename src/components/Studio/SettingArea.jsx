b04ae8cdcc58d964c1e134116c9892d8ede5d74b
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-17 15:00:58 +0900

    Feat: 옵션 선택에 따른 canvas 크기 동적 조절

--- src/components/Studio/SettingArea.jsx
+++ src/components/Studio/SettingArea.jsx
@@ -1,13 +1,38 @@
+import React from 'react';
 import useStore from '../../store/store';
-
 import ModalButton from '../shared/Button/ModalButton';
 import Logo from '../shared/Logo/Logo';
 
 function SettingArea() {
-  const { isModalOpened, setIsModalOpened, modalType, setModalType } =
-    useStore();
+  const {
+    isModalOpened,
+    setIsModalOpened,
+    modalType,
+    setModalType,
+    setCanvasSize,
+  } = useStore();
 
   const userName = 'leeraeroon';
+
+  const handleLayoutChange = (event) => {
+    const selectedValue = event.target.value;
+    switch (selectedValue) {
+      case 'option1':
+        setCanvasSize(210, 210, 210);
+        break;
+      case 'option2':
+        setCanvasSize(220, 220, 250);
+        break;
+      case 'option3':
+        setCanvasSize(300, 300, 300);
+        break;
+      case 'option4':
+        setCanvasSize(400, 400, 450);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="setting-area">
@@ -20,7 +45,7 @@ function SettingArea() {
       </div>
       <div className="layout-setting">
         <span>Layout</span>
-        <select className="layout-select">
+        <select className="layout-select" onChange={handleLayoutChange}>
           <option value="option1">210 x 210 x 210</option>
           <option value="option2">220 x 220 x 250</option>
           <option value="option3">300 x 300 x 300</option>
