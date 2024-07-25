465ffce93ae305d51ed8d51c02dbbe619b82d6ab
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-25 23:11:19 +0900

    Style: export 기능구현 전 로직 작성

--- src/components/Studio/ButtonArea.jsx
+++ src/components/Studio/ButtonArea.jsx
@@ -1,6 +1,24 @@
 import FileButton from '../shared/Button/FileButton';
 
 function ButtonArea() {
+  // const exportSTL = () => {
+  //   if (sceneRef.current) {
+  //     const exporter = new STLExporter();
+  //     const stlString = exporter.parse(sceneRef.current);
+  //     const blob = new Blob([stlString], { type: 'text/plain' });
+
+  //     const link = document.createElement('a');
+  //     link.style.display = 'none';
+  //     document.body.appendChild(link);
+
+  //     link.href = URL.createObjectURL(blob);
+  //     link.download = 'model.stl';
+  //     link.click();
+
+  //     document.body.removeChild(link);
+  //   }
+  // };
+
   return (
     <div className="button-area">
       <FileButton text="Save" />
