1fdc38de5d8796d5ca10ff64eb5567cf39add401
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-27 22:28:56 +0900

    Design: 이미지 추가 기능 별도 구성으로 레이아웃 변경

--- src/components/Studio/ToolBox.jsx
+++ src/components/Studio/ToolBox.jsx
@@ -1,4 +1,59 @@
+import { useRef } from 'react';
+
+import { PiImage } from 'react-icons/pi';
+
+import useStore from '../../store/store';
+
 function ToolBox({ type, iconList, selectTool, selectedTool }) {
+  const fileInputRef = useRef(null);
+  const { addLayer, setAlertState } = useStore();
+
+  const handleImageImport = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+          const layerData = {
+            type: 'image',
+            name: file.name,
+            width: img.width,
+            height: img.height,
+            x: 0,
+            y: 0,
+            rotation: 0,
+            visible: true,
+            opacity: 1,
+          };
+
+          addLayer(layerData);
+
+          setTimeout(() => {
+            const updatedLayers = useStore.getState().layerList;
+            const newLayer = updatedLayers[updatedLayers.length - 1];
+
+            if (newLayer && newLayer.id) {
+              try {
+                localStorage.setItem(file.name, e.target.result);
+              } catch (error) {
+                setAlertState('failed-image-import');
+              }
+            } else {
+              setAlertState('failed-image-import');
+            }
+          }, 0);
+        };
+        img.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="toolbox-container">
       {type === '2d' &&
@@ -12,6 +67,25 @@ function ToolBox({ type, iconList, selectTool, selectedTool }) {
             <IconComponent />
           </button>
         ))}
+      {type === '2d' && (
+        <>
+          <div className="divide-tool"></div>
+          <button
+            className="tool-button"
+            aria-label="image-button"
+            onClick={handleImageImport}
+          >
+            <PiImage />
+          </button>
+          <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </>
+      )}
       {type === '3d' &&
         iconList.map(({ id, icon: IconComponent }) => (
           <button
