9213583299b1a26ae6ca971f669b22de9d3fc54b
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-31 05:21:54 +0900

    Design: 잘못된 아이콘 수정

--- src/components/shared/Icon/viewRightIcon.jsx
+++ src/components/shared/Icon/viewRightIcon.jsx
@@ -9,11 +9,11 @@ function viewRightIcon() {
     >
       <path
         d="M3 17V6.5M3 17H13.5M3 17L6.5 13.5M3 6.5L6.5 3M3 6.5H13.5M6.5 3H17M6.5 3V13.5M17 3V13.5M17 3L13.5 6.5M17 13.5L13.5 17M17 13.5H6.5M13.5 17V6.5"
-        stroke="currentColor"
+        stroke="#2F2F3D"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <rect x="7" y="3" width="10" height="10" fill="currentColor" />
+      <path d="M14 6.40541V17L17 13.973V3L14 6.40541Z" fill="#2F2F3D" />
     </svg>
   );
 }
