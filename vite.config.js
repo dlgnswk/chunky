c927b28906763706688b23a4e7417f8841f740e8
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-08-19 14:18:12 +0900

    Chore: unit test 위한 의존성 설치

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    deps: {
+      inline: ['vitest-canvas-mock'],
+    },
+  },
 });
