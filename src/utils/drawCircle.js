04d048d770de88c658d34f4117f86d2a84deda90
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-08-23 18:57:30 +0900

    Remove: 사용되지 않는 함수 삭제

--- src/utils/drawCircle.js
+++
@@ -1,9 +0,0 @@
-const drawCircle = (ctx, center, radius, color, lineWidth) => {
-  ctx.beginPath();
-  ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = lineWidth;
-  ctx.stroke();
-};
-
-export default drawCircle;
